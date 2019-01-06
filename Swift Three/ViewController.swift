f5fe9dd62ca2c45ed509cac25e2663a860f41cdf
owen vallis <ov@cindys-macbook-pro.local>
2019-01-06 10:40:14 -0800

    Initial Commit

---
+++ Swift Three/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift Three
+//
+//  Created by owen vallis on 1/6/19.
+//  Copyright © 2019 owen b vallis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
