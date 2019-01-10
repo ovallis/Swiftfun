2df31a0d4b37ec9b55a319469bd9f7c143f5f870
owen vallis <ov@cindys-macbook-pro.local>
2019-01-10 08:04:22 -0800

    First commit point in git

--- Swift Three/ViewController.swift
+++ Swift Three/ViewController.swift
@@ -10,9 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var buttonCount = 0
+    @IBOutlet weak var MainLabel: UILabel!
+    
+    @IBAction func ChangeText(_ sender: Any) {
+        
+        buttonCount = buttonCount + 1
+        
+        print(buttonCount)
+        
+        
+        if buttonCount >= 10
+        {view.backgroundColor = UIColor.cyan
+            
+        MainLabel.text = "Hits > 10! Calorie Countdown Begins In 1 Min!"
+        }
+        
+        if buttonCount >= 15
+        {view.backgroundColor = UIColor.green
+            
+            MainLabel.text = "Hits > 15! Calorie Countdown Begins In 1 Min!"
+        }
+        
+        
+        
+        
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
     }
 
 
